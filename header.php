44011f6c572f35b86d779c93324fc6bffc1ad095
Joel <bpkjoel@gmail.com>
2021-06-29 22:25:24 -0400

    cambio4

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html lang="es">
 
 <head>
-    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +13,7 @@
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/materialize.min.css" media="screen,projection">
-
+    <link rel="icon" href="img/gota.png">
 </head>
 
 <body class="">
